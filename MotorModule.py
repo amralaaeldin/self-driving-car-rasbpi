8c3ab3d0383438e5620242997b78de67e9a760cb
Amr Alaaeldin <amralaaeldin.dev@gmail.com>
2022-10-16 10:09:19 +0200

    Adding different used modules

--- MotorModule.py
+++ MotorModule.py
@@ -19,15 +19,17 @@ class Motor():
         GPIO.setup(self.In1B, GPIO.OUT)
         GPIO.setup(self.In2B, GPIO.OUT)
         self.pwmA = GPIO.PWM(self.EnaA, 100)
-        self.pwmA.start(0)
         self.pwmB = GPIO.PWM(self.EnaB, 100)
+        self.pwmA.start(0)
         self.pwmB.start(0)
+        self.mySpeed = 0
 
     def move(self, speed=0.5, turn=0, t=0):
         speed *= 100
-        turn *= 100
-        leftSpeed = speed - turn
-        rightSpeed = speed + turn
+        turn *= 70
+        leftSpeed = speed-turn
+        rightSpeed = speed+turn
+
         if leftSpeed > 100:
             leftSpeed = 100
         elif leftSpeed < -100:
@@ -36,36 +38,38 @@ class Motor():
             rightSpeed = 100
         elif rightSpeed < -100:
             rightSpeed = -100
-
+        # print(leftSpeed,rightSpeed)
         self.pwmA.ChangeDutyCycle(abs(leftSpeed))
         self.pwmB.ChangeDutyCycle(abs(rightSpeed))
-
         if leftSpeed > 0:
             GPIO.output(self.In1A, GPIO.HIGH)
             GPIO.output(self.In2A, GPIO.LOW)
         else:
             GPIO.output(self.In1A, GPIO.LOW)
             GPIO.output(self.In2A, GPIO.HIGH)
-
         if rightSpeed > 0:
             GPIO.output(self.In1B, GPIO.HIGH)
             GPIO.output(self.In2B, GPIO.LOW)
         else:
             GPIO.output(self.In1B, GPIO.LOW)
             GPIO.output(self.In2B, GPIO.HIGH)
-
         sleep(t)
 
     def stop(self, t=0):
         self.pwmA.ChangeDutyCycle(0)
         self.pwmB.ChangeDutyCycle(0)
+        self.mySpeed = 0
         sleep(t)
 
 
 def main():
-    motor.move(0.6, 0, 2)
+    motor.move(0.5, 0, 2)
     motor.stop(2)
-    motor.move(-0.5, 0.2, 2)
+    motor.move(-0.5, 0, 2)
+    motor.stop(2)
+    motor.move(0, 0.5, 2)
+    motor.stop(2)
+    motor.move(0, -0.5, 2)
     motor.stop(2)
 
 
